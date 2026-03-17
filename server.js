77e091e629b149d10ea6e9a73cbfb039c184e973
ozgengursoy37-droid <ozgengursoy37@gmail.com>
2026-03-17 19:25:44 +0300

    Set up basic HTTP server to serve index.html

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res);
+});
+
+server.listen(process.env.PORT || 8080);
